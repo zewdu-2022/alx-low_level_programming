b876df4088fb457981bbe37fac69a321b7d67bfd
Zewdu Wondimagegn <user.name>
2022-04-10 19:06:57 +0300

    TASK 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0 after printin the function
  */
 int main(void)
 {
-puts(\"Programming is like building a multilingual puzzle");	
+puts("\"Programming is like building a multilingual puzzle");	
 return (0);
 }
