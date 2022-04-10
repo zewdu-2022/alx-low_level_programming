c9a17143b6d14f984055d182ccecb02d633c2e01
Zewdu Wondimagegn <user.name>
2022-04-10 22:01:40 +0300

    TASK 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,12 @@ int main(void)
 {
 char alphabet;
 
-for (alphabet = "a": alphabet <= "z": alphabet++) 
+for (alphabet = 'a': alphabet <= 'z': alphabet++) 
 {	
 putchar(alphabet);
 }
 
-putchar("\n");
+putchar('\n');
 
 return (0);
 
