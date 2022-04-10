a510c525493d6cd0c3a0ea33b3f541f3f440f027
Zewdu Wondimagegn <user.name>
2022-04-10 17:55:16 +0300

    TASK 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ long long int c;
 char d;
 float f;
 
-printf("size of a char: %|u byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a int: %|u byte(s)\n", (unsigned long)sizeof(a));
-printf("size of a long int: %|u byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long long: %|u byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a float: %|u byte(s)\n", (unsigned long)sizeof(f));
+printf("size of a char: 1 byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a int: 4 byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a long int: 4 byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long long: 8 byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a float: 4 byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
 
