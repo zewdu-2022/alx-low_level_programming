a9108df92df7f1416932f0c724290a14bbc00974
Zewdu Wondimagegn <user.name>
2022-04-11 16:04:29 +0300

    TASK 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <studio.h>
+/**
+ * main - print all possible combination
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int (digit1 = 0; digit1 < 9; digit1++)
+
+for (digit2n = digit + 1; digit2 <10; digit2++)
+{
+putchar(digit1 % 10 + '0');
+putchar(digit2 % 10 + '0');
+
+if (digit1 == 8 && digit2 == 9)
+continue;
+
+putchar(',')
+putchar(' ')
+
+returen (0);
+
+}
