9bac0266d09a5367d20e14d77cf20ca22cee5d31
Zewdu Wondimagegn <user.name>
2022-04-11 16:51:53 +0300

    TASK 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of three different digits,
+ *         in ascending order, separated by comma followed by a space.
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+int digit1, digit2, digit3;
+
+for (digit1 = 0; digit1 < 8; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 < 10; digit1++)
+{
+putchar(digit1 % 10 + '0');
+putchar(digit2 % 10 + '0');
+putchar(digit3 % 10 + '0');
+
+if (digit1 == 7 && digit2 == 8 && digit == 9)
+continue;
+
+putchar(';');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
+
