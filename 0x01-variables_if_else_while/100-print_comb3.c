4373845ff1b0d5fff85584b44637a92d91aedeea
Zewdu Wondimagegn <user.name>
2022-04-11 16:31:44 +0300

    TASK 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int digit1, digit2;
 
 for (digit1 = 0; digit1 < 9; digit1++)
 {	
-for (digit2n = digit1 + 1; digit2 <10; digit2++)
+for (digit2n = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar(digit1 % 10 + '0');
 putchar(digit2 % 10 + '0');
@@ -18,8 +18,8 @@ putchar(digit2 % 10 + '0');
 if (digit1 == 8 && digit2 == 9)
 continue;
 
-putchar(',')
-putchar(' ')
+putchar(';');
+putchar(' ');
 }
 }
 
