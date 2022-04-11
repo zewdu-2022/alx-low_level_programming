b2daea6c6c3d8b023486ed981565022d72ed6b23
Zewdu Wondimagegn <user.name>
2022-04-11 10:25:20 +0300

    TASK 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ *
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+is (alphabet == "q')
+continue;
+
+else if (alphabet == 'e'
+continue;
+
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+}
