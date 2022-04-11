144a32e1850eb9198082bffa5bd91fdad5ebc3ff
Zewdu Wondimagegn <user.name>
2022-04-11 18:12:10 +0300

    TASK 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int main(void)
 int n;
 
 srand(time(0));
-n = rand( ) - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 
 if (n == 0)
 {
@@ -27,7 +27,7 @@ printf("%i is negative\n", n);
 
 else 
 {
-prigntf("%i is positive\n", n);
+printf("%i is positive\n", n);
 }
 return (0);
 }
