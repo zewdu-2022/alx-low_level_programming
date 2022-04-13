381a4fe018e47ad0d40535c4d92e924457ee918b
Zewdu Wondimagegn <user.name>
2022-04-13 17:29:15 +0300

    TASK 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 	largest = a;
 	}
-	else if (b > c)
+	else if (b >= a && >= c)
 	{
 	largest = b;
 	}
@@ -26,4 +26,5 @@ int largest_number(int a, int b, int c)
 	}
 
 	return (largest);
+
 }
