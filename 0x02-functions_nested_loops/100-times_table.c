edb6d30d19e369dda1ff3b189406614dfc936f59
Zewdu Wondimagegn <user.name>
2022-05-19 14:46:38 +0300

    delet

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,7 @@
 void print_times_table(int n)
 {
         print_times_table(2);
+
 	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 0x00, C -hello, World
 This repository introduces C programming language
+0x0F. C - Variadic functions
